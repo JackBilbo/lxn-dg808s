459d90bf84f660cae30e2b55e6092fa5c0df953b
JackBilbo <abaelard@gmx.de>
2022-11-19 13:44:42 +0100

    Glide ratio calculation

--- touchingcloud-aircraft-dg808s-lxn/html_ui/Pages/VCockpit/Instruments/DG808S/nav/nav.js
+++ touchingcloud-aircraft-dg808s-lxn/html_ui/Pages/VCockpit/Instruments/DG808S/nav/nav.js
@@ -65,7 +65,9 @@ class lxn extends NavSystemTouch {
             wp_ete: { value: 0, label: "WP ETE", longlabel: "Waypoint Time Enroute", category: "time", baseunit: "min" },
             task_arr_agl: { value: 0, label: "TSK FIN (AGL)", longlabel: "Task Finish Altitude (AGL)", category: "alt", baseunit: "ft" },
             task_arr_msl: { value: 0, label: "TSK FIN (MSL)", longlabel: "Task Finish Altitude (MSL)", category: "alt", baseunit: "ft" },
-            task_spd: { value: 0, label: "TSK SPD", longlabel: "Task Speed", category: "speed", baseunit: "kts"}
+            task_spd: { value: 0, label: "TSK SPD", longlabel: "Task Speed", category: "speed", baseunit: "kts"},
+            current_gr: { value: 0, label: "GR", longlabel: "Glide Ratio", category: "plaintext", baseunit: "none"},
+            stf_gr: { value: 0, label: "STF GR", longlabel: "Glide Ratio at STF", category: "plaintext", baseunit: "none"}
         }
         
         this.units = {
@@ -283,6 +285,7 @@ class lxn extends NavSystemTouch {
             NAVPANEL.update();
             CONFIGPANEL.update();
             this.updateKineticAssistant();
+            this.calc_gr();
         }
 
         if(this.TIME_S - this.TIMER_1 > 1) {
@@ -1027,7 +1030,21 @@ class lxn extends NavSystemTouch {
         return false;
     }
 
+    calc_gr() {
+        /* Calculate current and optimum glide ratios */
+        this.vars.stf_gr.value = Math.abs(this.vars.stf.value / this.vars.sink_stf.value).toFixed(0);
 
+        let currentsink = SimVar.GetSimVarValue("VERTICAL SPEED", "knots");
+                
+        if(currentsink < 0) {
+            this.raw_gr = Math.abs(this.vars.ias.value / currentsink);
+            this.smoothed_gr = this.smoothed_gr ? this.smoothed_gr * 0.5 + this.raw_gr * 0.5 : this.raw_gr;
+            this.vars.current_gr.value = this.smoothed_gr.toFixed(0);
+        } else {
+            this.vars.current_gr.value = "---";
+            // this.smoothed_gr = null;
+        }
+    }
 
     /****************************************************************************************/
 
